e1f829020946bfbc36ffba03814b88c466656a28
rikkaka <793329010@qq.com>
2023-04-08 19:13:59 +0800

    修改：     Cargo.toml 	修改：     test.py

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ import rusql2py
 
 conn = rusql2py.connect("test.db") 
 conn.execute("CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)", [])
+conn.execute("INSERT INTO test (name) VALUES (?)", ["test"])
